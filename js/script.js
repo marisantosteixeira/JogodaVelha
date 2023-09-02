39c3d6c01f395c51255275da170b8a243fc31639
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-09-01 22:30:53 -0400

    DEU CERTO, OBG DEUS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const currentJogadores = document.querySelector(".currentJogadores");
 
-let selected;
+let marcar;
 let jogadores = "X";
 
 let positions = [
@@ -14,8 +14,8 @@ let positions = [
   [3, 5, 7],
 ];
 
-function init() {
-  selected = [];
+function iniciar() {
+    marcar = [];
 
   currentJogadores.innerHTML = `JOGADOR DA VEZ: ${jogadores}`;
 
@@ -25,41 +25,41 @@ function init() {
   });
 }
 
-init();
+iniciar();
 
 function newMove(e) {
   const index = e.target.getAttribute("data-i");
   e.target.innerHTML = jogadores;
   e.target.removeEventListener("click", newMove);
-  selected[index] = jogadores;
+  marcar[index] = jogadores;
 
   setTimeout(() => {
-    check();
+    checar();
   }, [100]);
 
   jogadores = jogadores === "X" ? "O" : "X";
-  currentJogadores.innerHTML = `JOGADOR DA VEZ: ${jogadores}`;
+  currentJogadores.innerHTML = `Vez do jogador: ${jogadores}`;
 }
 
-function check() {
+function checar() {
   let jogadoresLastMove = jogadores === "X" ? "O" : "X";
 
-  const items = selected
+  const items = marcar
     .map((item, i) => [item, i])
     .filter((item) => item[0] === jogadoresLastMove)
     .map((item) => item[1]);
 
   for (pos of positions) {
     if (pos.every((item) => items.includes(item))) {
-      document.write("O JOGADOR '" + jogadoresLastMove + "' GANHOU!");
-      init();
+      document.write("O jogador '" + jogadoresLastMove + "' venceu");
+      iniciar();
       return;
     }
   }
 
-  if (selected.filter((item) => item).length === 9) {
-    document.write("DEU EMPATE!");
-    init();
+  if (marcar.filter((item) => item).length === 9) {
+    document.write("ops, empatou legal");
+    iniciar();
     return;
 
   }
